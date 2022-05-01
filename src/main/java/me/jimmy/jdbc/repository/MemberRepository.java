57181d26db5223de616ead22d5bf3a38fd0796a3
JaeyeonKIM <foodsksms@gmail.com>
2022-05-01 20:23:57 +0900

    Feature 파일누락된거 추가

---
+++ src/main/java/me/jimmy/jdbc/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package me.jimmy.jdbc.repository;
+
+import me.jimmy.jdbc.domain.Member;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Member findById(String memberId);
+    void update(String memberId, int money);
+    void delete(String memberId);
+}
